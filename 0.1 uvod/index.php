e16fd1e741a01409f0f3101001980db9d41d4100
wansnerova <bimbawansner@gmail.com>
2026-09-10 11:22:34 +0200

    vytvoreny priecinok na premenne

--- 0.1 uvod/index.php
+++ 0.1 uvod/index.php
@@ -1,3 +1,21 @@
-<?php
-echo "Ahoj!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "pekny textik";
+    ?>
+    <div>
+        <?php
+        echo "dalsie echo";
+        ?>
+        <?php
+        print ("textik zas");
+        ?>
+    </div>
+</body>
+</html>
